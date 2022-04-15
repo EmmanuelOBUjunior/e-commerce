8557f2df4568f5da5ac14189e3c6b697754c14c0
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-04-15 22:06:10 +0000

    edited productc.jsx

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -10,7 +10,7 @@ const Product = ({product}) => {
 	<Card className = {classes.root}>
 		<CardMedia className= {classes.media} image = '' title = {product.name}/>
 		<CardContent>
-			<div className={classes.CardContent}>
+			<div className={classes.cardContent}>
 				<Typography variant = "h5" gutterBottom> 
 					{product.name}
 				</Typography>
