97c98e24439a2c48462cff056ced710bb77ec805
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-04-15 23:30:33 +0000

    import logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {AppBar, Toolbar, IconButton, Badge, Menu, MenuItem,Typography} from "@material-ui/core";
+import  {ShoppingCart} from "@material-ui/icons";
+ import logo from "../assets/Clest_logo1";
+
 
 const Navbar = () => {
   return (
-	<div>Navbar</div>
+	<>
+	<AppBar position = "fixed" className= {classes.appBar} color = "inherit">
+		<Toolbar>
+			<Typography variant = "h6" className = {classes.title} color = "ingerit">
+				<img src = {logo} alt = "DevShop" height = "25px" className={classes.image}/>
+				DevShop
+			</Typography>
+		</Toolbar>
+	</AppBar>
+	</>
   )
 }
 
