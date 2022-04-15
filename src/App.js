8093e3fd3f5dffadba8e85f68fb3e66d734334ca
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-04-15 22:09:05 +0000

    restructured grid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import Product from './components/Products/Product/Product'
+import Products from './components/Products/Products';
 
 const App = () => {
     return ( 
       <div>
-        <Product/>
+        <Products/>
       </div>
     )
 }
