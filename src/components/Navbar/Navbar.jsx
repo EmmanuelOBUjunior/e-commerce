ae44605212e9af8d2bc396b5f36d3a9177774991
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-04-16 14:04:46 +0000

    navbar not working

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 	<>
 	<AppBar position = "fixed" className= {classes.appBar} color = "inherit">
 		<Toolbar>
-			<Typography variant = "h6" className = {classes.title} color = "ingerit">
+			<Typography variant = "h6" className = {classes.title} color = "inherit">
 				<img src = {logo} alt = "DevShop" height = "25px" className={classes.image}/>
 				DevShop
 			</Typography>
