bc921d1e9d0c6f3cdddccb963a70e0587329734b
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-04-22 15:50:22 +0000

    Added styles.js and modified products.js

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import {Grid} from '@material-ui/core';
 import Product from './Product/Product';
 
+import useStyle from './styles';
+
 const products = [
 	{id: 1, name: "Shoe", description: "Running Shoe", price: "$5", image:"https://images.pexels.com/photos/601177/pexels-photo-601177.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
 	{id: 2, name: "Macbook", description: "Apple Macbook", price: "$10", image: "https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
 ];
 
 const Products = () =>{
+	const classes = useStyle(); 
 	return(
-	<main>
+	<main className={ classes.content}>
+		<div className ={classes.toolbar}/>
 		<Grid container justify = "center" spacing = {4}>
 			{products.map((products) => (
 				<Grid item key={products.id} xs = {12} sm = {6} md = {4} lg ={3}>
